b3d8626e807f092b5deadc413e49ced0c1cbd0ef
Arias800 <azertypoil630@gmail.com>
2019-07-09 10:42:19 +0200

    Update disney.py

--- plugin/disney.py
+++ plugin/disney.py
@@ -1,5 +1,9 @@
 #-*- coding: utf-8 -*-
+from kivy.properties import ObjectProperty
+
 from lib.comaddon import EXlog
+
+import re 
 
 SITE_IDENTIFIER = 'disneyhd_tk'
 SITE_NAME = 'Disney HD'
@@ -17,5 +21,30 @@ sPattern1 = '<a href="([^"]+)".+?src="([^"]+)" alt.*?="(.+?)".*?>'
 
 UA = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:56.0) Gecko/20100101 Firefox/56.0'
 
-def ShowPlugin():
-    EXlog('Disney Load')
+def ListMenu():
+    Menu = [('Liste Film',URL_LISTE),('Liste Film',URL_LISTE),('Liste Film',URL_LISTE),('Liste Film',URL_LISTE)]
+    EXlog('Menu '+str(Menu))
+    return Menu
+
+def getHtml(sUrl):
+    import urllib.parse
+    import urllib.request
+
+    headers = {'User-Agent': UA}
+
+    req = urllib.request.Request(sUrl, None, headers)
+    with urllib.request.urlopen(req) as response:
+       return response.read()
+
+def showMovies(sUrl):
+    html = getHtml(sUrl)
+
+    result = re.findall('<a href="([^"]+)".+?src="([^"]+)" alt.*?="(.+?)".*?>',str(html))
+    sTitle = []
+    sUrl = []
+
+    for res in result:
+        sUrl.append(URL_MAIN + res[0])
+        sTitle.append(res[2].replace('streaming', '').replace(' 1080p', '').replace('_', ' '))
+    content = dict(zip(sTitle,sUrl))
+    return content
