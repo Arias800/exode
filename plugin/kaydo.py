b3a48acc090dec2c50d55e40b63626fc1753c660
Arias800 <azertypoil630@gmail.com>
2019-07-10 14:33:50 +0200

    Update kaydo.py

--- plugin/kaydo.py
+++ plugin/kaydo.py
@@ -3,50 +3,90 @@ from kivymd.bottomsheet import MDListBottomSheet
 from lib.handler.requestHandler import cRequestHandler
 from lib.comaddon import EXlog
 
-import re 
+import re, json
 
 SITE_IDENTIFIER = 'kaydo'
 SITE_NAME = 'Kaydo'
 SITE_DESC = 'Kaydo : Films et serie en HD et en streaming'
 
 URL_MAIN = 'https://hdss.to/'
-URL_LISTE = URL_MAIN + 'films/'
-ANIM_ENFANTS = ('http://', 'load')
 
-#URL_SEARCH = ('', 'sHowResultSearch')
-#URL_SEARCH_MOVIES = ('', 'sHowResultSearch')
+URL_SEARCH = (URL_MAIN + 'search/avengers', 'showMovies')
+URL_SEARCH_MOVIES = (URL_SEARCH[0], 'showMovies')
+URL_SEARCH_SERIES = (URL_SEARCH[0], 'showMovies')
 FUNCTION_SEARCH = 'sHowResultSearch'
 
 UA = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:56.0) Gecko/20100101 Firefox/56.0'
 
-def ListMenu(self, kwargs):
-    bs = MDListBottomSheet()
-    Menu = [('Liste Film',URL_LISTE)]
-    for txt in Menu:
-        text = ("%s") % (txt[0])
-        bs.add_item(text, lambda x: self.PluginMenu(sName=str(kwargs['sName']),sCat=str(int(kwargs['sCat'])+1),sUrl=str(txt[1])))
-    bs.open()
-    return kwargs['sCat']
-
-def showMovies(self, kwargs):
-    bs = MDListBottomSheet()
-
-    oRequest = cRequestHandler(kwargs['sUrl'])
+def getJson():
+    oRequest = cRequestHandler(URL_MAIN + 'search/avengers')
     oRequest.addHeaderEntry('User-Agent', UA)
     sHtmlContent = oRequest.request()
 
-    result = re.findall('class="TPost C">.+?href="([^"]+)">.+?<img.+?src="([^"]+)".+?<h3 class="Title">([^<]+)</h3> .+?class="Qlty">([^<]+)<.+?<p>.+?streaming,([^<]+)',str(sHtmlContent), re.MULTILINE|re.DOTALL)
-    sTitle = []
-    sUrl = []
+    aResult = re.findall('class="TPost C">.+?href="([^"]+)">.+?<img.+?src="([^"]+)".+?<h3 class="Title">([^<]+)</h3> .+?class="Qlty">([^<]+)<.+?<p>.+?streaming,([^<]+)',str(sHtmlContent))
+    d1 = {"plugin":SITE_NAME}
 
-    for res in result:
-        sUrl.append(URL_MAIN + res[0])
-        sTitle.append(res[2])
-    content = dict(zip(sTitle,sUrl))
+    i = 0
+    for aEntry in aResult:
+        if i >= 2:
+            break
 
-    for aEntry in content.items():
-        EXlog(content)
-        text = ("%s") % (aEntry[0])
-        bs.add_item(text, lambda x: self.PluginMenu(sName=str(kwargs['sName']), sCat=str(int(kwargs['sCat'])+1)))
-    bs.open()
-    return kwargs['sCat']
+        sUrl = aEntry[0]
+        qual = aEntry[3]
+        sQual,url = getFinalUrl(sUrl,qual)
+
+        for qual,sUrl in zip(sQual,url):
+            sTitle = aEntry[2]
+            extra = ({"source":{'title' : sTitle,"url":sUrl,"qual":qual}})
+            d1.update(extra)
+        i = i+1
+
+    JSON = json.dumps(d1)
+
+    return JSON
+
+def getFinalUrl(sUrl,qual=''):
+
+    oRequestHandler = cRequestHandler(sUrl)
+    sHtmlContent = oRequestHandler.request()
+
+    oRequestHandler = cRequestHandler(sUrl)
+    sHtmlContent = oRequestHandler.request()
+
+    #Recuperer variable pour url de base
+    aResult = re.findall('id=.+?trembed=([^"]+).+?frameborder',str(sHtmlContent))
+
+    url = []
+    sQual = []
+
+    for aEntry in aResult:
+        EXlog(aEntry.replace('#038;',''))
+
+        site = URL_MAIN + "?trembed=" + aEntry.replace('#038;','')
+        oRequestHandler = cRequestHandler(site)
+        sHtmlContent = oRequestHandler.request()
+
+        #Recuperation de l'url suivante
+        aResult = re.search('<div class="Video"><iframe.+?src="([^"]+)"',str(sHtmlContent)).group(1)
+
+        Url = ''.join(aResult)
+        oRequestHandler = cRequestHandler(Url)
+        sHtmlContent = oRequestHandler.request()
+
+        #Recuperation de l'id
+        aResult1 = re.search("var id.+?'(.+?)'",str(sHtmlContent)).group(1)
+        sPost = ''.join(aResult1)[::-1]
+        sUrl1 = URL_MAIN + '?trhidee=1&trfex=' + sPost
+
+        oRequestHandler = cRequestHandler(sUrl1)
+        oRequestHandler.addHeaderEntry('Referer', Url)
+        sHtmlContent = oRequestHandler.request()
+        sHosterUrl = oRequestHandler.getRealUrl()
+
+        if 'public/dist' in sHosterUrl:
+            sHosterUrl = 'https://' + sHosterUrl.split('/')[2] + '/hls/'+sHosterUrl.split('id=')[1] + '/'+sHosterUrl.split('id=')[1] + '.playlist.m3u8'
+
+        url.append(sHosterUrl)
+        sQual.append(qual)
+
+        return sQual,url
