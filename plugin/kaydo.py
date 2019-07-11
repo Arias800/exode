22ed95885020bda184e0d2a20f724f1ec41c4b7e
Arias800 <azertypoil630@gmail.com>
2019-07-11 16:13:39 +0200

    Update kaydo.py

--- plugin/kaydo.py
+++ plugin/kaydo.py
@@ -18,30 +18,30 @@ FUNCTION_SEARCH = 'sHowResultSearch'
 
 UA = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:56.0) Gecko/20100101 Firefox/56.0'
 
-def getJson():
-    oRequest = cRequestHandler(URL_MAIN + 'search/avengers')
+def getJson(title):
+    oRequest = cRequestHandler(URL_MAIN + 'search/'+title)
     oRequest.addHeaderEntry('User-Agent', UA)
     sHtmlContent = oRequest.request()
 
     aResult = re.findall('class="TPost C">.+?href="([^"]+)">.+?<img.+?src="([^"]+)".+?<h3 class="Title">([^<]+)</h3> .+?class="Qlty">([^<]+)<.+?<p>.+?streaming,([^<]+)',str(sHtmlContent))
     d1 = {"plugin":SITE_NAME}
 
+    dest = []
+
     i = 0
     for aEntry in aResult:
-        if i >= 2:
-            break
 
         sUrl = aEntry[0]
         qual = aEntry[3]
+        sTitle = aEntry[2]
         sQual,url = getFinalUrl(sUrl,qual)
 
         for qual,sUrl in zip(sQual,url):
-            sTitle = aEntry[2]
             extra = ({"source":{'title' : sTitle,"url":sUrl,"qual":qual}})
-            d1.update(extra)
-        i = i+1
+            dest.append((d1))
+            dest.append((extra))
 
-    JSON = json.dumps(d1)
+    JSON = json.dumps(dest)
 
     return JSON
 
@@ -50,32 +50,28 @@ def getFinalUrl(sUrl,qual=''):
     oRequestHandler = cRequestHandler(sUrl)
     sHtmlContent = oRequestHandler.request()
 
-    oRequestHandler = cRequestHandler(sUrl)
-    sHtmlContent = oRequestHandler.request()
-
     #Recuperer variable pour url de base
-    aResult = re.findall('id=.+?trembed=([^"]+).+?frameborder',str(sHtmlContent))
+    aResult1 = re.findall('id=.+?trembed=([^"]+)(?:"|&quot;) frameborder',str(sHtmlContent))
 
     url = []
     sQual = []
 
-    for aEntry in aResult:
-        EXlog(aEntry.replace('#038;',''))
+    for aEntry in aResult1:
+        site = URL_MAIN + "?trembed=" + aEntry.replace('#038;','').replace('amp;','')
 
-        site = URL_MAIN + "?trembed=" + aEntry.replace('#038;','')
         oRequestHandler = cRequestHandler(site)
         sHtmlContent = oRequestHandler.request()
 
         #Recuperation de l'url suivante
-        aResult = re.search('<div class="Video"><iframe.+?src="([^"]+)"',str(sHtmlContent)).group(1)
+        aResult2 = re.search('<div class="Video"><iframe.+?src="([^"]+)"',str(sHtmlContent)).group(1)
 
-        Url = ''.join(aResult)
+        Url = ''.join(aResult2)
         oRequestHandler = cRequestHandler(Url)
         sHtmlContent = oRequestHandler.request()
 
         #Recuperation de l'id
-        aResult1 = re.search("var id.+?'(.+?)'",str(sHtmlContent)).group(1)
-        sPost = ''.join(aResult1)[::-1]
+        aResult3 = re.search("var id.+?'(.+?)'",str(sHtmlContent)).group(1).replace('\\','')
+        sPost = ''.join(aResult3)[::-1]
         sUrl1 = URL_MAIN + '?trhidee=1&trfex=' + sPost
 
         oRequestHandler = cRequestHandler(sUrl1)
@@ -89,4 +85,4 @@ def getFinalUrl(sUrl,qual=''):
         url.append(sHosterUrl)
         sQual.append(qual)
 
-        return sQual,url
+    return sQual,url
